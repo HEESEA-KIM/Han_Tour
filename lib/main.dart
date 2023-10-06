41390f9824c95b64efd3e839b6621c5124922c6b
HyunJunLee <guswns0414@naver.com>
2023-10-06 14:32:26 +0900

    코드 리펙토링

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hanstour/navigationrail_custom.dart';
 
-void main() {
-  runApp(const NavigationRailExampleApp());
-}
+void main() => runApp(NavigationRailExampleApp());
 
 class NavigationRailExampleApp extends StatelessWidget {
   const NavigationRailExampleApp({super.key});
@@ -28,79 +26,37 @@ class _NavRailExampleState extends State<NavRailExample> {
   int _selectedIndex = 0;
   bool _extended = true;
 
+  List<NavigationRailDestination> get _navRailDestinations {
+    final icons = [
+      Icons.airplanemode_active,
+      Icons.paragliding,
+      Icons.route,
+      Icons.airplane_ticket,
+      Icons.hotel
+    ];
+    final labels = ["Tour", "Activity", "Transport", "Ticket", "Hotel"];
+    return List.generate(icons.length, (index) {
+      return buildCustomNavigationRailDestination(
+        icon: icons[index],
+        isExtended: _extended,
+        label: labels[index],
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
-        children: <Widget>[
+        children: [
           Stack(
             children: [
               NavigationRail(
                 minWidth: _extended ? 121 : 40,
                 selectedIndex: _selectedIndex,
-                onDestinationSelected: (int index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                leading: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (_extended)
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(220, 80),
-                          backgroundColor: Colors.white,
-                          elevation: 0.0,
-                        ),
-                        onPressed: () {},
-                        child: Image.asset(
-                          'assets/nomadMateLogo.png',
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    IconButton(
-                      icon: Icon(
-                        _extended
-                            ? Icons.arrow_circle_left
-                            : Icons.arrow_circle_right,
-                        color: Colors.blue,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _extended = !_extended;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                destinations: <NavigationRailDestination>[
-                  buildCustomNavigationRailDestination(
-                    icon: Icons.airplanemode_active,
-                    isExtended: _extended,
-                    label: "Tour",
-                  ),
-                  buildCustomNavigationRailDestination(
-                    icon: Icons.paragliding,
-                    isExtended: _extended,
-                    label: "Activity",
-                  ),
-                  buildCustomNavigationRailDestination(
-                    icon: Icons.route,
-                    isExtended: _extended,
-                    label: "Transport",
-                  ),
-                  buildCustomNavigationRailDestination(
-                    icon: Icons.airplane_ticket,
-                    isExtended: _extended,
-                    label: "Ticket",
-                  ),
-                  buildCustomNavigationRailDestination(
-                    icon: Icons.hotel,
-                    isExtended: _extended,
-                    label: "Hotel",
-                  ),
-                ],
+                onDestinationSelected: (index) => setState(() => _selectedIndex = index),
+                leading: _buildLeading(),
+                destinations: _navRailDestinations,
               ),
               Positioned(
                 top: 150.0 + (_selectedIndex * 120.0),
@@ -113,70 +69,88 @@ class _NavRailExampleState extends State<NavRailExample> {
               ),
             ],
           ),
+          Expanded(child: _buildTabContent()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLeading() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (_extended)
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(220, 80),
+              backgroundColor: Colors.white,
+              elevation: 0.0,
+            ),
+            onPressed: () {},
+            child: Image.asset(
+              'assets/nomadMateLogo.png',
+              fit: BoxFit.fill,
+            ),
+          ),
+        IconButton(
+          icon: Icon(
+            _extended ? Icons.arrow_circle_left : Icons.arrow_circle_right,
+            color: Colors.blue,
+          ),
+          onPressed: () => setState(() => _extended = !_extended),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTabContent() {
+    return DefaultTabController(
+      length: 3,
+      child: Column(
+        children: [
+          Container(
+            color: Colors.grey[200],
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 60),
+            child: _buildTabBar(),
+          ),
           Expanded(
-            child: DefaultTabController(
-              length: 3,
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.grey[200],
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 50,
-                      vertical: 60,
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: TabBar(
-                        indicatorSize: TabBarIndicatorSize.tab,
-                        indicator: ShapeDecoration(
-                          color: Colors.blue,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                        ),
-                        unselectedLabelColor: Colors.black26,
-                        labelColor: Colors.white,
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                        indicatorColor: Colors.blue,
-                        tabs: [
-                          Tab(text: 'RECOMMENDED'),
-                          Tab(text: 'TOP RATED'),
-                          Tab(text: 'LOWEST PRICE'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.grey[200],
-                      child: TabBarView(
-                        children: [
-                          Center(child: Text('Tab 1 Content')),
-                          Center(child: Text('Tab 2 Content')),
-                          Center(child: Text('Tab 3 Content')),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            child: Container(
+              color: Colors.grey[200],
+              child: _buildTabBarView(),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildTabBar() {
+    final tabs = ['RECOMMENDED', 'TOP RATED', 'LOWEST PRICE'];
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), spreadRadius: 2, blurRadius: 5, offset: Offset(0, 3))],
+      ),
+      child: TabBar(
+        indicatorSize: TabBarIndicatorSize.tab,
+        indicator: ShapeDecoration(color: Colors.blue, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
+        unselectedLabelColor: Colors.black26,
+        labelColor: Colors.white,
+        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+        indicatorColor: Colors.blue,
+        tabs: tabs.map((e) => Tab(text: e)).toList(),
+      ),
+    );
+  }
+
+  Widget _buildTabBarView() {
+    return TabBarView(
+      children: [
+        Center(child: Text('Tab 1 Content')),
+        Center(child: Text('Tab 2 Content')),
+        Center(child: Text('Tab 3 Content')),
+      ],
     );
   }
 }
