288a809c0b88a96ebce59262db8070cfa7419cf3
HyunJunLee <guswns0414@naver.com>
2023-10-28 18:00:32 +0900

    예약정보 UI 변경

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -71,7 +71,7 @@ class _ReservationDetailsPageState extends State<ReservationDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: SizedBox(height: 300,
+      child: SizedBox(height: 500,
         child: Scaffold(
           appBar: AppBar(title: Text("Reservation Details"),
           centerTitle: true,
@@ -92,17 +92,27 @@ class _ReservationDetailsPageState extends State<ReservationDetailsPage> {
                   style: TextStyle(
                     fontSize: 25,fontWeight: FontWeight.bold,
                   ),),
+                SizedBox(height: 30,),
                 Text("Name: ${reservationDetails!['userName']}",
                   style: TextStyle(
                     fontSize: 20,
                   ),),
-                Text("Location${reservationDetails!['lacation']}",
+                SizedBox(height: 30,),
+                Text("Location: \n\n${reservationDetails!['location']}",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20,
                 ),
-
-
-                // 다른 필드들에 대한 정보도 추가할 수 있습니다.
+                ),
+                SizedBox(height: 50,),
+              Text("Contact: 1661-2000",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+              ),
+              SizedBox(height: 30,),
+              ElevatedButton(onPressed: (){}, child: Text('Cancel'),),
               ],
             )
                 : CircularProgressIndicator(),
